f97caf148fcf40d6ea6e722b7629fde68aab7679
Nguyen Minh Khanh <khanhnm222@gmail.com>
2025-09-10 21:33:53 +0700

    feat: upgrade @ui5 version to latest

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext", // to support top-level await
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
 });
