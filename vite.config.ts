c7a41dde655486158c52d3460e4416472f193647
Nguyen Minh Khanh <khanhnm222@gmail.com>
2025-09-21 18:13:15 +0700

    feat(app configs): add configurations for aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,24 @@ export default defineConfig({
       target: "esnext",
     },
   },
+  resolve: {
+    alias: {
+      '@root': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@configs': path.resolve(__dirname, 'src/configs'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@libs': path.resolve(__dirname, 'src/libs'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@providers': path.resolve(__dirname, 'src/providers'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@routers': path.resolve(__dirname, 'src/routers'),
+    },
+  },
+  server: {
+    hmr: {
+      overlay: true,
+    },
+  }
 });
