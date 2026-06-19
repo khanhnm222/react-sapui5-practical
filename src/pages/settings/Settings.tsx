74d7c30ed00041c2e7a7dbed6d9e5dabec5f3eb8
Nguyen Minh Khanh <khanhnm222@gmail.com>
2026-06-20 05:40:24 +0700

    feat: create settings page

---
+++ src/pages/settings/Settings.tsx
@@ -0,0 +1,11 @@
+import { Title, Card } from '@ui5/webcomponents-react';
+
+const Settings = () => {
+  return (
+    <Card header={<Title>Settings</Title>}>
+      <p>Update application settings here.</p>
+    </Card>
+  );
+};
+
+export default Settings;
